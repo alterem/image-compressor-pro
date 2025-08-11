53bbf08b5f0a794589e8725c3b7f1e662786e527
Alterem <alterem520@gmail.com>
2025-08-11 19:15:45 +0800

    fix: 修正资源路径，确保图标和清单文件正确加载

--- src/components/CompressIcon.tsx
+++ src/components/CompressIcon.tsx
@@ -7,7 +7,7 @@ interface CompressIconProps {
 export const CompressIcon: React.FC<CompressIconProps> = ({ className = "w-6 h-6" }) => {
   return (
     <img
-      src="./icon-512.svg"
+      src="icon-512.svg"
       alt="图片压缩"
       className={className}
     />
